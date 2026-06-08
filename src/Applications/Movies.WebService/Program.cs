f59699d372ef03414b0a9a836a247b5eaca2e0a4
felipe d ferreira <felipedferreira11@gmail.com>
2026-06-07 23:49:26 -0400

    chore: update project path for Movies.WebService.Contracts and clean up Program.cs

--- src/Applications/Movies.WebService/Program.cs
+++ src/Applications/Movies.WebService/Program.cs
@@ -61,9 +61,7 @@ public class Program
             {
                 options.EnabledClients = [ScalarClient.HttpClient, ScalarClient.Axios, ScalarClient.Fetch];
                 options.EnabledTargets = [ScalarTarget.CSharp, ScalarTarget.JavaScript];
-
                 options.Theme = ScalarTheme.Solarized;
-                
                 options.Favicon = "/favicon.ico";
                 options.EndpointPathPrefix = "/api-docs/{documentName}";
                 options.Title = "API Documentation - {documentName}";
